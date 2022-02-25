77b94f2ea08b5c18a0b4e4279929248b739957c9
wudstrand <wudstrand@auddia.dev>
2022-02-25 14:32:01 -0700

    t

--- actions/compile_openapi/build_openapi_yaml.sh
+++ actions/compile_openapi/build_openapi_yaml.sh
@@ -18,8 +18,9 @@ cd $OPENAPI_DATA_DIR
 
 touch temp.yaml
 sed "s/$ENV_KEY/$ENV_VALUE/g" $TEMPLATE > temp.yaml
-swagger-cli bundle -o /$HOME/$OUTFILE -t yaml -r  temp.yaml
+swagger-cli bundle -o $HOME/$OUTFILE -t yaml -r  temp.yaml
 
+echo $HOME
 ls $HOME
 # cat ./temp.yaml
 # cat /output/$OUTFILE
