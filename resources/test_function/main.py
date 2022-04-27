2546f1f5726f4c004fdb51ca24a594c80c46e322
Will Udstrand <will.udstrand@gmail.com>
2022-04-27 15:31:17 -0600

    Updating function for testing

--- resources/test_function/main.py
+++ resources/test_function/main.py
@@ -1,11 +1,13 @@
 import json
 import logging
+import os
 
 
 def handle(request):
     """
     """
     logging.info("Test log message")
+    logging.info(f"ONE: {os.getenv('ONE')}, TWO: {os.getenv('TWO')}")
     return {
         "status": "OK",
         "description": "Test Function"
