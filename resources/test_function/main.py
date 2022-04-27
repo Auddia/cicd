961433dbe1161e293f7405a962435b2cfaf58453
Will Udstrand <will.udstrand@gmail.com>
2022-04-27 16:00:16 -0600

    Debugging issues

--- resources/test_function/main.py
+++ resources/test_function/main.py
@@ -1,14 +1,11 @@
-import json
-import logging
 import os
 
 
 def handle(request):
     """
     """
-    logging.info("Test log message")
-    logging.info(f"ONE: {os.getenv('ONE')}, TWO: {os.getenv('TWO')}")
     return {
         "status": "OK",
-        "description": "Test Function"
+        "description": "Test Function",
+        "details": f"ONE: {os.getenv('ONE')}, TWO: {os.getenv('TWO')}"
     }
